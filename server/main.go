b2e56fe3b452f6986abd9bfe20d6c769a33b2689
JoelFaldin <joelfaldin@gmail.com>
2026-07-09 23:58:06 -0400

    feat: add listenPublic function to accept internet visitors

--- server/main.go
+++ server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net"
 	"sync"
@@ -10,14 +11,17 @@ import (
 var (
 	controlMu  sync.Mutex
 	controlEnc *json.Encoder
+
+	pendingMu sync.Mutex
+	pending   = map[string]net.Conn{}
 )
 
 func main() {
-	go listen()
+	go listenControl()
 }
 
 // Accepts an incoming request from ngrok lite
-func listen() {
+func listenControl() {
 	ln, err := net.Listen("tcp", ":4443")
 	if err != nil {
 		log.Fatal(err)
@@ -28,7 +32,7 @@ func listen() {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Println("[control] Error: ", err)
+			log.Println("[control] Acept error: ", err)
 			continue
 		}
 
@@ -37,5 +41,51 @@ func listen() {
 		controlMu.Lock()
 		controlEnc = json.NewEncoder(conn)
 		controlMu.Unlock()
+	}
+}
+
+// Accepts an internet visitor. For each one, ask the client (control)
+// to open a join connection
+func listenPublic() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("[public] Listening on :8080!")
+
+	var counter int
+	for {
+		visitor, err := ln.Accept()
+		if err != nil {
+			log.Println("[public] Accept error: ", err)
+			continue
+		}
+
+		controlMu.Lock()
+		enc := controlEnc
+		controlMu.Unlock()
+
+		if enc == nil {
+			log.Println("[public] No client connected, rejecing visitor...")
+			visitor.Close()
+			continue
+		}
+
+		counter++
+		id := fmt.Sprintf("%d", counter)
+
+		pendingMu.Lock()
+		pending[id] = visitor
+		pendingMu.Unlock()
+
+		log.Println("[public] Visitor", visitor.RemoteAddr(), "-> id", id)
+
+		controlMu.Lock()
+		err = enc.Encode(protocol.Message{Type: "new_conn", ID: id})
+		controlMu.Unlock()
+		if err != nil {
+			log.Println("[control] Couldnt report to client:", err)
+		}
 	}
 }
