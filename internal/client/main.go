7420ce4f39379f6f64d28c75cb26b9390b918f40
JoelFaldin <joelfaldin@gmail.com>
2026-07-21 23:48:29 -0400

    feat: implement handleNewConn on client and fix client and server files to run as separate apps

--- internal/client/main.go
+++ internal/client/main.go
@@ -1,19 +1,20 @@
-package client
+package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net"
 	"nsky/internal/protocol"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "3000:4443")
+	conn, err := net.Dial("tcp", "localhost:4443")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("[client] Connecting to control channel in 3000:4443")
+	log.Println("[client] Connecting to control channel in localhost:4443")
 	log.Println("[client] Forwarding traffic to :3000")
 
 	dec := json.NewDecoder(conn)
@@ -33,5 +34,21 @@ func main() {
 }
 
 func handleNewConn(id, join, local string) {
+	joinConn, err := net.Dial("tcp", join)
+	if err != nil {
+		log.Fatalf("Couldnt open join conn: %v", err)
+	}
+
+	fmt.Fprintf(joinConn, "%s\n", id)
+
+	localConn, err := net.Dial("tcp", local)
+	if err != nil {
+		log.Println("Couldnt connect to local app: ", err)
+		joinConn.Close()
+		return
+	}
+
+	log.Println("Stream ", id, " -> proxing towards ", local)
+	pipe(joinConn, localConn)
 
 }
